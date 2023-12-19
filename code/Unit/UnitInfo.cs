4c75d1c2a4d652ee88aea6d8f9c07e804f2ed960
yuberee <59583743+yuberee@users.noreply.github.com>
2023-12-19 21:17:14 +0100

    UnitInfo damage and kill, apply relevant tags

--- code/Unit/UnitInfo.cs
+++ code/Unit/UnitInfo.cs
@@ -8,4 +8,25 @@ public sealed class UnitInfo : Component
 
 	[Property]
 	public List<UnitType> EnemyUnitTypes { get; set; }
+
+	protected override void OnStart()
+	{
+		base.OnStart();
+
+		base.Tags.Set( "Unit", true ); // Give the Unit tag
+		base.Tags.Set( UnitType.ToString(), true ); // Give tag of whatever unit type this is
+	}
+
+	public void Damage( float amount )
+	{
+		Health = Math.Max( Health - amount, 0 );
+
+		if ( Health <= 0 )
+			Kill();
+	}
+
+	public void Kill()
+	{
+		GameObject.Destroy();
+	}
 }
