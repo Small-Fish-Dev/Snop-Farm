da90d60c915e16c5527cfe514d188cfbff60372e
yuberee <59583743+yuberee@users.noreply.github.com>
2023-12-23 15:47:00 +0100

    Add ScaleByHealth

--- code/Unit/UnitInfo.cs
+++ code/Unit/UnitInfo.cs
@@ -4,10 +4,20 @@ public class UnitInfo : Component
 	public float Health { get; set; } = 10f;
 
 	[Property]
+	public bool ScaleByHealth { get; set; } = false;
+
+	[Range( 0f, 1f, 0.1f )]
+	[Property]
+	public float MinScale { get; set; } = 0.2f;
+
+	[Property]
 	public UnitType UnitType { get; set; } = UnitType.None;
 
 	[Property]
 	public ModelRenderer Renderer { get; set; }
+
+	[Property]
+	public string HurtAnimationVariable { get; set; } = "hit";
 
 	[Property]
 	public List<UnitType> EnemyUnitTypes { get; set; }
@@ -16,6 +26,8 @@ public class UnitInfo : Component
 	public float HurtAnimationDuration => Math.Max( LastDamage / 10f, 0.05f );
 	public TimeSince LastHurt { get; set; } = float.MaxValue;
 	public bool HurtAnimation => LastHurt != float.MaxValue && LastHurt <= HurtAnimationDuration;
+	public float MaxHealth;
+	public Vector3 MaxScale;
 
 
 	protected override void OnStart()
@@ -24,6 +36,9 @@ public class UnitInfo : Component
 
 		base.Tags.Set( "Unit", true ); // Give the Unit tag
 		base.Tags.Set( UnitType.ToString(), true ); // Give tag of whatever unit type this is
+
+		MaxHealth = Health;
+		MaxScale = Transform.Scale;
 	}
 
 	protected override void OnUpdate()
@@ -45,7 +60,13 @@ public class UnitInfo : Component
 	{
 		Health = Math.Max( Health - amount, 0 );
 
+		if ( ScaleByHealth )
+			Transform.Scale = MaxScale * MathX.Remap( Health, 0f, MaxHealth, MinScale, 1f );
+
 		HurtFX();
+
+		if ( Renderer is SkinnedModelRenderer renderer )
+			renderer.Set( HurtAnimationVariable, true );
 
 		if ( Health <= 0 )
 			Kill();
